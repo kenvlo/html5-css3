d14b6f912df8a9a95f6ab1af071b14864ec25449
Ken Lo <ken456878@gmail.com>
2020-05-30 01:40:32 +0800

    up to 3-2

--- js/index.js
+++ js/index.js
@@ -13,4 +13,40 @@ window.onload = function () {
   function hideKeyword() {
     document.getElementById("searchSuggestion").style.display = "none";
   }
+
+  bannerOption();
+  function bannerOption() {
+    var swiper = document.getElementById("swiper");
+    var swiperItem = swiper.getElementsByClassName("swiperItem");
+    var prev = document.getElementsByClassName("prev")[0];
+    var next = document.getElementsByClassName("next")[0];
+    var index = 0;
+
+    for (var i = 0; i < swiperItem.length; i++) {
+      if (index == i) {
+        swiperItem[i].style.opacity = 1;
+      } else {
+        swiperItem[i].style.opacity = 0;
+      }
+      swiperItem[i].style.transform =
+        "translateX(" + (-i * swiperItem[0].offsetWidth) + "px)";
+    }
+
+    prev.onclick = function () {
+      index--;
+      changeImg();
+    };
+
+    next.onclick = function () {
+      index++;
+      changeImg();
+    };
+
+    function changeImg() {
+      for (var j = 0; j < swiperItem.length; j++) {
+        swiperItem[j].style.opacity = 0;
+      }
+      swiperItem[index].style.opacity = 1;
+    }
+  }
 };
